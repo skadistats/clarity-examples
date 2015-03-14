7d259d3ed489f85c9e4bb78af200e1a5edbb5393
Martin Schrodt <martin@schrodt.org>
2015-03-14 13:56:41 +0100

    schee macht's :)

--- src/main/java/skadistats/clarity/examples/two/simple/Main.java
+++ src/main/java/skadistats/clarity/examples/two/simple/Main.java
@@ -1,7 +1,6 @@
 package skadistats.clarity.examples.two.simple;
 
 import com.dota2.proto.Netmessages;
-import com.google.protobuf.GeneratedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import skadistats.clarity.two.processor.reader.OnMessage;
@@ -15,8 +14,14 @@ public class Main {
     public static class Test {
         @OnMessage(Netmessages.CSVCMsg_CreateStringTable.class)
         public void message(Context context, Netmessages.CSVCMsg_CreateStringTable message) {
-            System.out.println(message.getName());
+            System.out.println("create " + message.getName());
         }
+
+        @OnMessage(Netmessages.CSVCMsg_UpdateStringTable.class)
+        public void message(Context context, Netmessages.CSVCMsg_UpdateStringTable message) {
+            System.out.println("update + "+ message.getTableId());
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -26,7 +31,6 @@ public class Main {
         Logger log = LoggerFactory.getLogger("simple");
 
         new Runner().runWith(new FileInputStream(args[0]), new Test());
-
 
         long tMatch = System.currentTimeMillis() - tStart;
         log.info("total time taken: {}s", (tMatch) / 1000.0);
