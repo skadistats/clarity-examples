b34809a5b2095cc09eec1841af55d5792a6d92bc
Martin Schrodt <martin@schrodt.org>
2015-03-15 08:56:16 +0100

    log level adjustments

--- src/main/java/skadistats/clarity/examples/simple/Main.java
+++ src/main/java/skadistats/clarity/examples/simple/Main.java
@@ -1,6 +1,6 @@
 package skadistats.clarity.examples.simple;
 
-import com.dota2.proto.Usermessages;
+import com.dota2.proto.DotaUsermessages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import skadistats.clarity.two.processor.reader.OnMessage;
@@ -9,23 +9,26 @@ import skadistats.clarity.two.processor.runner.Runner;
 
 import java.io.FileInputStream;
 
+
 public class Main {
 
-    public static class Test {
-        @OnMessage(Usermessages.CUserMsg_SayText2.class)
-        public void doIt(Context ctx, Usermessages.CUserMsg_SayText2 m) {
-            System.out.println(ctx.getTick());
-            System.out.println(m.toString());
-        }
+    @OnMessage(DotaUsermessages.CDOTAUserMsg_ChatWheel.class)
+    public void doIt(Context ctx, DotaUsermessages.CDOTAUserMsg_ChatWheel m) {
+        System.out.println(ctx.getTick());
+        System.out.println(m.toString());
+    }
+
+    private final Logger log = LoggerFactory.getLogger(Main.class.getPackage().getClass());
+
+    public void run(String[] args) throws Exception {
+        long tStart = System.currentTimeMillis();
+        new Runner().runWith(new FileInputStream(args[0]), this);
+        long tMatch = System.currentTimeMillis() - tStart;
+        log.info("total time taken: {}s", (tMatch) / 1000.0);
     }
 
     public static void main(String[] args) throws Exception {
-        long tStart = System.currentTimeMillis();
-        Logger log = LoggerFactory.getLogger("simple");
-        new Runner().runWith(new FileInputStream(args[0]), new Test());
-        long tMatch = System.currentTimeMillis() - tStart;
-        log.info("total time taken: {}s", (tMatch) / 1000.0);
-        
+        new Main().run(args);
     }
 
 }
