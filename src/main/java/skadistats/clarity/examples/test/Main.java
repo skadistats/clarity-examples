ac108e21bcaf53e735747573481f18d4c7f45d1f
Martin Schrodt <martin@schrodt.org>
2015-08-02 15:37:07 +0200

    add S2 packet types add unpacker for embedded packets in S2

--- src/main/java/skadistats/clarity/examples/test/Main.java
+++ src/main/java/skadistats/clarity/examples/test/Main.java
@@ -1,10 +1,9 @@
 package skadistats.clarity.examples.test;
 
-import com.google.protobuf.ByteString;
 import com.google.protobuf.GeneratedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import skadistats.clarity.processor.reader.OnMessageContainer;
+import skadistats.clarity.processor.reader.OnMessage;
 import skadistats.clarity.processor.runner.Context;
 import skadistats.clarity.processor.runner.SimpleRunner;
 import skadistats.clarity.source.MappedFileSource;
@@ -13,9 +12,9 @@ public class Main {
 
     private final Logger log = LoggerFactory.getLogger(Main.class.getPackage().getClass());
 
-    @OnMessageContainer
-    public void onFullPacket(Context ctx, Class<? extends GeneratedMessage> messageClass, ByteString data) {
-        System.out.println(messageClass.getName());
+    @OnMessage
+    public void onFullPacket(Context ctx, GeneratedMessage message) {
+        //System.out.println(message.getClass().getName());
     }
 
     public void run(String[] args) throws Exception {
