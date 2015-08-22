1e605bfa635ece752c574a325f92094184e926f3
Martin Schrodt <martin@schrodt.org>
2015-08-22 18:15:32 +0200

    sync

--- src/main/java/skadistats/clarity/examples/test/Main.java
+++ src/main/java/skadistats/clarity/examples/test/Main.java
@@ -116,7 +116,7 @@ public class Main {
 
     @OnTickStart
     public void onTickStart(Context ctx, boolean synthetic) throws InterruptedException, FileNotFoundException, UnsupportedEncodingException {
-        if (ctx.getTick() == 50000) {
+        if (ctx.getTick() == 10000) {
             //System.out.println(new HuffmanGraph(FieldPathDecoder.HUFFMAN_TREE).generate());
             StringTables stringTables = ctx.getProcessor(StringTables.class);
             DTClasses dtClasses = ctx.getProcessor(DTClasses.class);
