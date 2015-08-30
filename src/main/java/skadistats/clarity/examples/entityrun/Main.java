405138f281cf9905335ed66cfded6c2f06adfb33
Martin Schrodt <martin@schrodt.org>
2015-08-30 16:04:16 +0200

    houston, we have a liftoff!

--- src/main/java/skadistats/clarity/examples/entityrun/Main.java
+++ src/main/java/skadistats/clarity/examples/entityrun/Main.java
@@ -19,7 +19,12 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        new Main().run(args);
+        try {
+            new Main().run(args);
+        } catch (Exception e) {
+            Thread.sleep(200);
+            throw e;
+        }
     }
 
 }
