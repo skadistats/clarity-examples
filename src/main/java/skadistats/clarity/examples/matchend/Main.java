e712acff40de328fcb2ab2953944b1e3435100be
Martin Schrodt <martin@schrodt.org>
2015-09-09 20:36:23 +0200

    implement getFieldPathForName()

--- src/main/java/skadistats/clarity/examples/matchend/Main.java
+++ src/main/java/skadistats/clarity/examples/matchend/Main.java
@@ -2,6 +2,8 @@ package skadistats.clarity.examples.matchend;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import skadistats.clarity.decoder.Util;
+import skadistats.clarity.model.EngineType;
 import skadistats.clarity.model.Entity;
 import skadistats.clarity.model.FieldPath;
 import skadistats.clarity.processor.entities.Entities;
@@ -23,9 +25,43 @@ public class Main {
         ControllableRunner r = new ControllableRunner(new MappedFileSource(args[0])).runWith(this);
         r.seek(r.getLastTick());
         r.halt();
-        summary(r.getContext());
+        if (r.getEngineType() == EngineType.SOURCE1) {
+            summary(r.getContext());
+        } else {
+            summary2(r.getContext());
+        }
         long tMatch = System.currentTimeMillis() - tStart;
         log.info("total time taken: {}s", (tMatch) / 1000.0);
+    }
+
+    private void summary2(Context ctx) throws UnsupportedEncodingException {
+        Entity ps = ctx.getProcessor(Entities.class).getByDtName("CDOTA_PlayerResource");
+
+        String[][] columns = new String[][]{
+            {"Name", "m_vecPlayerData.%i.m_iszPlayerName"},
+            {"Level", "m_vecPlayerTeamData.%i.m_iLevel"},
+            {"K", "m_vecPlayerTeamData.%i.m_iKills"},
+            {"D", "m_vecPlayerTeamData.%i.m_iDeaths"},
+            {"A", "m_vecPlayerTeamData.%i.m_iAssists"}
+        };
+
+        TextTable.Builder b = new TextTable.Builder();
+        for (int c = 0; c < columns.length; c++) {
+            b.addColumn(columns[c][0], c == 0 ? TextTable.Alignment.LEFT : TextTable.Alignment.RIGHT);
+        }
+        TextTable t = b.build();
+
+        for (int c = 0; c < columns.length; c++) {
+            for (int r = 0; r < 10; r++) {
+                FieldPath fp = ps.getDtClass().getFieldPathForName(columns[c][1].replace("%i", Util.arrayIdxToString(r)));
+                Object val = ps.getPropertyForFieldPath(fp);
+                String str = new String(val.toString().getBytes("ISO-8859-1"));
+                t.setData(r, c, str);
+            }
+        }
+
+        System.out.println(t);
+
     }
 
     private void summary(Context ctx) throws UnsupportedEncodingException {
