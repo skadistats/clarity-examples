60c4b55bf059d3717948e09fefdc8fad187962df
Martin Schrodt <martin@schrodt.org>
2023-05-27 14:24:01 +0300

    fix: remove debugging code and improve example

--- src/main/java/skadistats/clarity/examples/dump/Main.java
+++ src/main/java/skadistats/clarity/examples/dump/Main.java
@@ -3,23 +3,31 @@ package skadistats.clarity.examples.dump;
 import com.google.protobuf.GeneratedMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import skadistats.clarity.Clarity;
 import skadistats.clarity.processor.reader.OnMessage;
 import skadistats.clarity.processor.runner.SimpleRunner;
 import skadistats.clarity.source.MappedFileSource;
+import skadistats.clarity.wire.csgo.s1.proto.CSGOS1NetMessages;
+import skadistats.clarity.wire.dota.s1.proto.DOTAS1NetMessages;
+import skadistats.clarity.wire.shared.s2.proto.S2NetMessages;
 
 public class Main {
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class.getPackage().getClass());
-    private static boolean OUTPUT = false;
+    private final Logger log = LoggerFactory.getLogger(Main.class.getPackage().getClass());
 
     @OnMessage(GeneratedMessage.class)
     public void onMessage(GeneratedMessage message) {
-        if (OUTPUT) {
-            log.info(message.getClass().getName());
-            log.info(message.toString());
+        if (isAudio(message)) {
+            return;
         }
-        OUTPUT = false;
+        log.info(message.getClass().getName());
+        log.info(message.toString());
+    }
+
+    private boolean isAudio(GeneratedMessage message) {
+        return
+                message instanceof DOTAS1NetMessages.CSVCMsg_VoiceData
+                || message instanceof CSGOS1NetMessages.CSVCMsg_VoiceData
+                || message instanceof S2NetMessages.CSVCMsg_VoiceData;
     }
 
     public void run(String[] args) throws Exception {
@@ -30,25 +38,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        String[][] data = {
-                {"DOTA_S1", "/home/spheenik/projects/replays/dota/s1/normal/271145478.dem"},
-                {"DOTA_S2", "/home/spheenik/projects/replays/dota/s2/297/7048355297.dem"},
-                {"CSGO_S1", "/home/spheenik/projects/replays/csgo/s1/issue-271/astralis-vs-godsent-m1-nuke.dem"},
-                {"CSGO_S2", "/home/spheenik/projects/replays/csgo/s2/prelaunch/bayes_demo.dem"}
-        };
-        for (String[] datum : data) {
-            OUTPUT = true;
-            System.out.println(datum[0]);
-            new Main().run(new String[] {datum[1]});
-        }
-        for (String[] datum : data) {
-            System.out.println(datum[0]);
-            try {
-                System.out.println(Clarity.infoForFile(datum[1]));
-            } catch (Exception e) {
-                log.error("Info not found", e);
-            }
-        }
+        new Main().run(args);
     }
 
 }
